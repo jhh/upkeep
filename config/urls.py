42fd0e898c4711974bb4fa56901e2062e6de3b54
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-21 12:09:34 -0500

    Add task detail page
    Former-commit-id: fc7536a663dd63e70eed7b6e216cf6aca87b4d53

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
 
-from upkeep.core.views import areas_view, tasks_view
+from upkeep.core.views import areas_view, task_view, tasks_view
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/areas"), name="home"),
     path("areas/", areas_view, name="areas"),
     path("tasks/", tasks_view, name="tasks"),
+    path("tasks/<int:pk>/", task_view, name="task"),
     path("admin/", admin.site.urls),
 ]
 
