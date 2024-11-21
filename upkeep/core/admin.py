57dc285b8a08a2fc79fa9cc4f67fe1fed96c690c
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-21 06:05:32 -0500

    Update models
    Former-commit-id: 16eea797b5bf5620a082325c2d0b02e48348ee92

--- upkeep/core/admin.py
+++ upkeep/core/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Area, Consumable, Schedule, Task, TaskHistory
+from .models import Area, Consumable, Schedule, Task, TaskConsumable
 
 admin.site.register(Area)
-admin.site.register(Task)
 admin.site.register(Consumable)
+admin.site.register(TaskConsumable)
 admin.site.register(Schedule)
-admin.site.register(TaskHistory)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["area__name", "name"]
+    list_display_links = ["name"]
+    list_select_related = True
