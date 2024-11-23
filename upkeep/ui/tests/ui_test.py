76522cab830b1939d2e747e600160d6d2887fae7
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-23 06:59:05 -0500

    Fixes for nix flake check
    Former-commit-id: 3a8ccfb308fdfd02c4e8a5b2312b1b506fbf2e7e

--- upkeep/ui/tests/ui_test.py
+++ upkeep/ui/tests/ui_test.py
@@ -3,4 +3,4 @@ from __future__ import annotations
 
 def test_home_page_status_code(client):
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 302
