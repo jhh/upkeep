fe14a2c465c78a592b1ff91f24105c035fc28807
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-23 13:22:18 -0500

    Optimize areas list queries

--- upkeep/core/views.py
+++ upkeep/core/views.py
@@ -11,7 +11,29 @@ from .models import Area, Task
 
 def areas_view(request):
     if request.method == "GET":
-        areas = Area.objects.annotate(task_count=Count("tasks")).all()
+        area_queryset = (
+            Area.objects.prefetch_related("tasks__schedules")
+            .annotate(task_count=Count("tasks"))
+            .all()
+        )
+
+        areas = []
+        for area in area_queryset:
+            row = {
+                "id": area.id,
+                "name": area.name,
+                "task_count": area.task_count,
+            }
+
+            schedules = []
+            for task in area.tasks.all():
+                schedules += task.schedules.filter(completion_date__isnull=True).all()
+
+            if schedules:
+                first = min(schedules, key=lambda s: s.due_date)
+                row.update({"due_date": first.due_date, "due_task_id": first.task_id})
+
+            areas.append(row)
 
         breadcrumbs = [
             {"name": "Home", "url": None},
