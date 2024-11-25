35226e8adac255cab79dfd0ee64d85c2d3976b32
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-25 14:09:31 -0500

    Enhance Schedule admin
    Former-commit-id: 45b83b8104cce84f35d832748ce37f02469a09c5

--- upkeep/core/admin.py
+++ upkeep/core/admin.py
@@ -5,11 +5,17 @@ from .models import Area, Consumable, Schedule, Task, TaskConsumable
 admin.site.register(Area)
 admin.site.register(Consumable)
 admin.site.register(TaskConsumable)
-admin.site.register(Schedule)
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ["area__name", "name"]
     list_display_links = ["name"]
+    list_select_related = True
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ["task__area__name", "task__name", "due_date", "completion_date"]
+    list_display_links = ["due_date", "completion_date"]
     list_select_related = True
