1ace822ca39be10670753f16eb144476169afa0c
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-27 14:07:38 -0500

    Add Consumable edit form

--- upkeep/core/forms.py
+++ upkeep/core/forms.py
@@ -4,7 +4,7 @@ from crispy_forms.layout import HTML, Div, Field, Layout, Submit
 from django import forms
 from django.urls import reverse
 
-from upkeep.core.models import Area, Schedule, Task
+from upkeep.core.models import Area, Consumable, Schedule, Task
 
 
 class AreaForm(forms.ModelForm):
@@ -123,7 +123,6 @@ class ScheduleForm(forms.ModelForm):
         self.fields["task"].choices = [(t[0], f"{t[1]}: {t[2]}") for t in tasks]  # type: ignore
 
         self.helper.layout = Layout(
-            # Hidden("task", value=task_id),
             Field("task"),
             Field("due_date", css_class="form-control"),
             Field("completion_date", css_class="form-control"),
@@ -143,6 +142,52 @@ class ScheduleForm(forms.ModelForm):
                 HTML(f"""<button type="button" class="btn btn-outline-danger ms-auto"
                 hx-delete="{{% url 'schedule_edit' {self.instance.id} %}}"
                 hx-confirm="Delete this schedule?"
+                >Delete</button>""")
+                if is_edit
+                else None,
+                css_class="d-flex",
+            ),
+        )
+
+
+class ConsumableForm(forms.ModelForm):
+    class Meta:
+        model = Consumable
+        fields = ["name", "notes", "quantity", "unit"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        is_edit = self.instance.id is not None
+
+        self.helper = FormHelper()
+        self.helper.render_hidden_fields = True
+        self.helper.form_method = "post"
+        self.helper.form_action = (
+            reverse("consumable_edit", args=[self.instance.id])
+            if is_edit
+            else reverse("consumable_new")
+        )
+
+        self.helper.layout = Layout(
+            Field("name", css_class="form-control"),
+            Field("quantity", css_class="form-control"),
+            Field("unit", css_class="form-control"),
+            Field("notes", css_class="form-control"),
+            Div(
+                Submit(
+                    "submit",
+                    "Update" if is_edit else "Save",
+                    css_class="btn btn-primary",
+                ),
+                StrictButton(
+                    "Cancel",
+                    name="cancel",
+                    css_class="btn-secondary ms-2",
+                    onclick="window.history.back();",
+                ),
+                HTML(f"""<button type="button" class="btn btn-outline-danger ms-auto"
+                hx-delete="{{% url 'consumable_edit' {self.instance.id} %}}"
+                hx-confirm="Delete this consumable?"
                 >Delete</button>""")
                 if is_edit
                 else None,
